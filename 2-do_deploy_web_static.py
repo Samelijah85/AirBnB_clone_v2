9691c1877b42fb335b796e092d10c72e9b19a33e
AbuArwa001 <khalfanathman12@yahoo.com>
2024-04-04 13:57:50 +0300

    Distribute archive - extract file name bug

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,6 +3,7 @@
  of the web_static folder of AirBnB Clone repo"""
 from fabric.api import local, task, env, put, run  # Import the task decorator
 from datetime import datetime
+import os
 
 env.hosts = ['ubuntu@34.204.81.253', 'ubuntu@52.87.154.218']
 
@@ -20,32 +21,36 @@ def do_pack():
     return None
 
 
-def do_deploy(path):
+def do_deploy(archive_path):
     """ distributes an
     archive to your web servers, using the function do_deploy"""
     # Ensure path is provided
-    if not path:
+    if not archive_path:
         return False
     else:
         # Extract the base filename without the .tgz extension
-        result = run(f'filename=$(basename -s .tgz {path})')
-        if result.failed:
-            return False
+        base_name = os.path.basename(archive_path)
+        # Split the base name into root and extension (e.g.,
+        # 'web_static_20240404115939', '.tgz')
+        filename, _ = os.path.splitext(base_name)
+        # result = run(f'filename=$(basename -s .tgz {archive_path})')
+        # if result.failed:
+        #     return False
 
         # Upload the .tgz file to /tmp directory on the remote server
-        result = put(f'{path}', '/tmp')
+        result = put(f'{archive_path}', '/tmp')
         if result.failed:
             return False
 
         # Extract the uploaded .tgz file to
         # /data/web_static/releases/<filename> directory
-        result = run(f'tar -xvzf /tmp/$filename.tgz -C \
+        result = run(f'tar -xvzf /tmp/{filename}.tgz -C \
                      /data/web_static/releases/$filename')
         if result.failed:
             return False
 
         # Remove the uploaded .tgz file from /tmp directory
-        result = run(f'rm /tmp/$filename.tgz')
+        result = run(f'rm /tmp/{filename}.tgz')
         if result.failed:
             return False
 
@@ -53,10 +58,11 @@ def do_deploy(path):
         result = run(f'rm /data/web_static/current')
         if result.failed:
             return False
+        run(f'echo {filename}')
 
         # Create a symlink to the newly deployed version
-        result = run(f'ln -s /data/web_static/releases/$filename \
-                     /data/web_static/current')
+        result = run(f'ln -s /data/web_static/releases/{filename}\
+    /data/web_static/current')
         if result.failed:
             return False
 

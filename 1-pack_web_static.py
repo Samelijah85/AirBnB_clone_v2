865f63b60f4fcb075e512cc2c91441812de8d2a1
EvanieWares <evaniewares@gmail.com>
2024-04-05 05:44:43 +0200

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Script that generates a .tgz archive from the contents
- of the web_static folder of AirBnB Clone repo"""
-from fabric.api import local, task, env  # Import the task decorator
+"""
+Script that generates a .tgz archive from the contents of the web_static
+folder of AirBnB Clone repo
+"""
+from fabric.api import local
 from datetime import datetime
-
-env.hosts = ['ubuntu@54.88.80.89', 'ubuntu@54.160.119.162']
+import os.path
 
 
 def do_pack():
@@ -16,10 +17,15 @@ def do_pack():
     """
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     archive_path = "versions/web_static_{}.tgz".format(date)
-    local("mkdir -p versions")
-    result = local("tar -cvzf {} web_static".format(archive_path))
-
-    if result.succes:
-        print("web_static packed: {}".format(archive_path))
-        return archive_path
-    return None
+    print("Packing web_static to {}".format(archive_path))
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(archive_path)).failed is True:
+        return None
+    print("web_static packed: {} -> {}Bytes".format(
+        archive_path,
+        os.path.getsize(archive_path)
+        )
+    )
+    return archive_path

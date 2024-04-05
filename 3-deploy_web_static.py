753f9aba09e69fdb2a61a2999890ddaa963cc0c1
EvanieWares <evaniewares@gmail.com>
2024-04-05 05:55:20 +0200

    Full deployment - fix path

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,9 +3,9 @@
 Script that generates a .tgz archive from the contents
 of the web_static folder of AirBnB Clone repo
 """
-from fabric.api import local, task, env, put, run  # Import the task decorator
+from fabric.api import local, env, put, run
 from datetime import datetime
-import os
+import os.path
 
 env.hosts = ['ubuntu@54.88.80.89', 'ubuntu@54.160.119.162']
 
@@ -19,13 +19,18 @@ def do_pack():
     """
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     archive_path = "versions/web_static_{}.tgz".format(date)
-    local("mkdir -p versions")
-    result = local("tar -cvzf {} web_static".format(archive_path))
-
-    if result.succes:
-        print("web_static packed: {}".format(archive_path))
-        return archive_path
-    return None
+    print("Packing web_static to {}".format(archive_path))
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(archive_path)).failed is True:
+        return None
+    print("web_static packed: {} -> {}Bytes".format(
+        archive_path,
+        os.path.getsize(archive_path)
+        )
+    )
+    return archive_path
 
 
 def do_deploy(archive_path):
@@ -37,55 +42,37 @@ def do_deploy(archive_path):
     Returns:
         True if all operations have been done correctly, otherwise False.
     """
-    # Ensure path is provided
-    if not archive_path:
+    if os.path.isfile(archive_path) is False:
         return False
-    else:
-        # Extract the base filename without the .tgz extension
-        base_name = os.path.basename(archive_path)
-        # Split the base name into root and extension (e.g.,
-        # 'web_static_20240404115939', '.tgz')
-        filename, _ = os.path.splitext(base_name)
-        # result = run(f'filename=$(basename -s .tgz {archive_path})')
-        # if result.failed:
-        #     return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-        result = run(f'mkdir -p /data/web_static/releases/{filename}')
-        if result.failed:
-            return False
-        # Upload the .tgz file to /tmp directory on the remote server
-        result = put(f'{archive_path}', '/tmp')
-        if result.failed:
-            return False
-
-        # Extract the uploaded .tgz file to
-        # /data/web_static/releases/<filename> directory
-        result = run(f'tar -xzf /tmp/{filename}.tgz -C \
-                     /data/web_static/releases/{filename}')
-        if result.failed:
-            return False
-
-        # Remove the uploaded .tgz file from /tmp directory
-        result = run(f'mv  /data/web_static/releases/{filename}/web_static/* \
-                      /data/web_static/releases/{filename}/')
-        result = run(f'rm /tmp/{filename}.tgz')
-        result = run(f'rm -rf \
-            /data/web_static/releases/{filename}/web_static/')
-        if result.failed:
-            return False
-
-        # Remove the current symlink
-        result = run(f'rm -rf /data/web_static/current')
-        if result.failed:
-            return False
-
-        # Create a symlink to the newly deployed version
-        result = run(f'ln -s /data/web_static/releases/{filename}\
-    /data/web_static/current')
-        if result.failed:
-            return False
-        print('New version deployed!')
-        return True
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    print('New version deployed!')
+    return True
 
 
 def deploy():

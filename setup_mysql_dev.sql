a2663042dadebe517f8dfa1dc3334e853bd424d0
EvanieWares <evaniewares@gmail.com>
2024-04-22 06:59:55 +0200

    updated setup_mysql_dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,12 +1,5 @@
--- Create database if it doesn't exist
+-- Create a user with all privileges
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Create user if they don't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Grant privileges to the user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
--- Flush privileges
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION
